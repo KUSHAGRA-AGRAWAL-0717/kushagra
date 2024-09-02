22895bcb0b79ed8a8276187ae4c89a8e9700bdac
KUSHAGRA-AGRAWAL-0717 <157118982+KUSHAGRA-AGRAWAL-0717@users.noreply.github.com>
2024-09-03 01:02:40 +0530

    Updated navbar responsiveness and font family

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,52 +1,40 @@
 import "./navbar.scss";
 import { Link } from "react-scroll";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <div className="container">
-      <Link
-        to="home"
-        smooth={true}
-        duration={500}
-        style={{ textDecoration: "none" }}
-      >
+      <Link to="home" smooth={true} duration={500} style={{ textDecoration: "none" }}>
         <div className="search">
           <p>KUSHAGRA</p>
           <p>AGRAWAL</p>
         </div>
       </Link>
 
-      <div className="second">
-        <Link
-          to="home"
-          smooth={true}
-          duration={500}
-          style={{ textDecoration: "none" }}
-        >
+      <div className="menu-icon" onClick={toggleMenu}>
+        <div className={isMenuOpen ? "line open" : "line"}></div>
+        <div className={isMenuOpen ? "line open" : "line"}></div>
+        <div className={isMenuOpen ? "line open" : "line"}></div>
+      </div>
+
+      <div className={`second ${isMenuOpen ? "open" : ""}`}>
+        <Link to="home" smooth={true} duration={500} style={{ textDecoration: "none" }}>
           <div className="item">Home</div>
         </Link>
-        <Link
-          to="about"
-          smooth={true}
-          duration={500}
-          style={{ textDecoration: "none" }}
-        >
+        <Link to="about" smooth={true} duration={500} style={{ textDecoration: "none" }}>
           <div className="item">About</div>
         </Link>
-        <Link
-          to="projects"
-          smooth={true}
-          duration={500}
-          style={{ textDecoration: "none" }}
-        >
+        <Link to="projects" smooth={true} duration={500} style={{ textDecoration: "none" }}>
           <div className="item">Projects</div>
         </Link>
-        <Link
-          to="contact"
-          smooth={true}
-          duration={500}
-          style={{ textDecoration: "none" }}
-        >
+        <Link to="contact" smooth={true} duration={500} style={{ textDecoration: "none" }}>
           <div className="item">Contact</div>
         </Link>
       </div>
